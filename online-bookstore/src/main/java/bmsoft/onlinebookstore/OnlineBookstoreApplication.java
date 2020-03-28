4f7d308e01c4b234cae0357f0b09341a842aac63
Anas Ali Qureshi <Anas Ali Qureshi@DESKTOP-GJOUO8E>
2020-03-28 13:28:28 -0700

    Initial Commit Created, Spring Boot Project and configure the application.properties

---
+++ online-bookstore/src/main/java/bmsoft/onlinebookstore/OnlineBookstoreApplication.java
@@ -0,0 +1,14 @@
+package bmsoft.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreApplication.class, args);
+		System.out.println("Hellow World");
+	}
+
+}
