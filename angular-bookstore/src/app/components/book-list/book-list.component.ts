10dc075caaaafd86be0df8a4755fee368291eb95
Anas Ali Qureshi <Anas Ali Qureshi@DESKTOP-GJOUO8E>
2020-04-10 15:55:46 -0700

    Completed the feature 3 search by category.

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/app/services/book.service';
 import { Book } from 'src/app/common/book';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
@@ -9,18 +10,34 @@ import { Book } from 'src/app/common/book';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  
-  books : Book[];
-  constructor(private _bookService: BookService) { }
 
-  ngOnInit (): void{
-    this.listbooks();
-    
+  books: Book[];
+  currentCategoryId: number;
+
+  constructor(private _bookService: BookService,
+    private _activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe(() => {
+      this.listbooks();
+    })
+
   }
 
-  listbooks(){
-     this._bookService.getBooks().subscribe(
-     data => this.books = data
+  listbooks() {
+
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+
+    if (hasCategoryId){
+      this.currentCategoryId = + this._activatedRoute.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+
+
+    }
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
+      data => this.books = data
     )
   }
 }
